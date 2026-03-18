39f6975ceb71d26f52e6e567a662d756ad8b968f
GaneshDhakali <ganeshrdhakli08@gmail.com>
2026-03-19 00:25:39 +0530

    Fixed env error

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URI);
   console.log("Mongo Connected");
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
+require("dotenv").config({ path: "./backend/.env"});
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const movieRoutes = require("./routes/movieRoutes");
 const errorHandler = require("./middleware/errorHandler");
+const PORT = process.env.PORT || 5000;
 
 
-
-dotenv.config();
 connectDB();
 
 const app = express();
@@ -19,3 +19,4 @@ app.use("/api/movies", require("./routes/movieRoutes"));
 
 app.listen(5000, () => console.log("Server Running"));
 app.use(errorHandler);
+console.log("ENV VALUE:", process.env.MONGO_URI);
